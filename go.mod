280d0303cfec58f671411420823c80ea69a84ecd
freecoder-dev <freecoderr@gmail.com>
2023-05-20 17:03:08 +0200

    add server status checker code.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module project_name
+module server-status
 
 go 1.19
--- src/app/main.go
+++ src/app/main.go
@@ -1,9 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+	"time"
+)
 
 var Revision string
 
+type Site struct {
+	Number       int
+	URL          string
+	HTTPCode     int
+	ResponseTime time.Duration
+	StatusMsg    string
+}
+
+func CheckSiteStatus(site *Site) {
+	start := time.Now()
+	resp, err := http.Get(site.URL)
+	elapsed := time.Since(start)
+
+	if err != nil {
+		site.HTTPCode = -1
+		site.StatusMsg = "NOK"
+	} else {
+		site.HTTPCode = resp.StatusCode
+		site.StatusMsg = strings.Split(resp.Status, " ")[1]
+	}
+
+	site.ResponseTime = elapsed.Round(time.Millisecond)
+}
+
 func main() {
-	fmt.Printf("Hello freecoder's!: %s\n", Revision)
+	fmt.Println("Server Status Checker:")
+	fmt.Println()
+	fmt.Println("No.\tURLs\t\t\tCode\tTime\tStatus")
+
+	sites := []Site{
+		{URL: "https://google.com"},
+		{URL: "https://apple.com"},
+		{URL: "https://yahoo.com"},
+		{URL: "https://freecoder.dev"},
+	}
+
+	for i, site := range sites {
+		site.Number = i + 1
+		CheckSiteStatus(&site)
+		fmt.Printf("%d\t%s\t%d\t%.2fs\t%s\n",
+			site.Number,
+			site.URL,
+			site.HTTPCode,
+			site.ResponseTime.Seconds(),
+			site.StatusMsg)
+	}
 }
